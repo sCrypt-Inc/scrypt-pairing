985ac6850f7f22f5c09fa0f969f246eaa554f5f7
mihasinkec <sinkec.mihael@gmail.com>
2022-07-05 19:52:32 +0200

    Progress, currently implementing and test FQ6 ops.

---
+++ bn256/test_pairing.py
@@ -0,0 +1,31 @@
+import os
+import json
+import random
+
+from scryptlib import (
+        compile_contract, build_contract_class, build_type_classes
+        )
+
+
+if __name__ == '__main__':
+    contract = 'testPairing.scrypt' 
+
+    compiler_result = compile_contract(contract, debug=False)
+    desc = compiler_result.to_desc()
+
+    # Load desc instead:
+    #with open('./out/testCurve_desc.json', 'r') as f:
+    #    desc = json.load(f)
+
+    type_classes = build_type_classes(desc)
+    CurvePoint = type_classes['CurvePoint']
+    TwistPoint = type_classes['TwistPoint']
+    FQ12Point = type_classes['FQ12Point']
+
+    BN256PairingTest = build_contract_class(desc)
+    bn256_pairing_test = BN256PairingTest()
+
+    #assert bn256_pairing_test.testMiller(
+
+    #).verify()
+
